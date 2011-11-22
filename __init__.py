017f995a2facd336dde6deb69103d35b1c2c40c1
Samuel John <github@samueljohn.de>
2011-11-22 14:47:48 +0100

    doc/license added

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+'''
+A datasource (based on https://github.com/samueljohn/datasource) that
+reads CSV values from an eyetracking experiement and supplies the data in a
+convenient manner.
+
+There is a datasource that can "fake" eyetracking data for a quite simple case.
+The ShiftEyeTrackingData and JerkEyeTrackingData can be used to simulate 
+systematic errors in the datasource.
+
+@author: 
+    Samuel John, Erik Weitnauer
+'''
 from eyedata import T, X, Y, FixationData, FixationDataFromCSV, EyeTrackerDataSource
 from fakedata import EyeTrackerFakeDataSource
 from corruptdata import ShiftEyeTrackingData, JerkEyeTrackingData
