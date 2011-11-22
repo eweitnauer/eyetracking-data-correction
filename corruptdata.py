cfe4fea9d5204f9866c0aee452cb09bba0b462cd
Samuel John <github@samueljohn.de>
2011-11-22 10:22:48 +0100

    Added license stuff and short description to corruptdata.py

--- corruptdata.py
+++ corruptdata.py
@@ -1,3 +1,26 @@
+'''
+Bratty distortion of 2D eyetracking data. 
+
+@copyright: 
+    2011 Samuel John
+@author: 
+    Samuel John
+@contact: 
+    www.SamuelJohn.de
+@license: 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
 import mdp
 import scipy as S
 from eyetracker_data import T, X, Y
