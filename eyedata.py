5219bc1e2ec3d1a795c1a793f7696de5b610d684
eweitnau <eweitnauer@gmail.com>
2011-12-05 18:21:06 +0100

    EyeTrackerDataSource has attribute ranges now which it reads from FixationDataFromCSV

--- eyedata.py
+++ eyedata.py
@@ -109,6 +109,7 @@ class EyeTrackerDataSource(DS.DataSource):
     '''A data source for the trial of a single person for one eye.'''
     def __init__(self, fixation_data=None, trial_id=0, person_id=0, eye=None, **kws):
         self.data      = fixation_data
+        self.ranges = fixation_data.ranges
         self.trial_id  = trial_id
         self.person_id = person_id
         self._reset()
